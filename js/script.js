736ab62fc060e3d391c6f9c82b51efe5d0aa45a2
Elena Shagaeva <30930203+tommiknocker@users.noreply.github.com>
2017-09-09 11:25:15 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,5 @@
+ 		var searchButton = document.querySelector(".button");
+	 	var openForm = document.querySelector(".form");
+	 	searchButton.addEventListener("click", function (evt) {
+	 	evt.preventDefault();
+	 	openForm.classList.toggle("form-show");});
