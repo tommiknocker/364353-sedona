ca25283848291b457a2ffbc7fa1dcafbc74141f2
Elena Shagaeva <30930203+tommiknocker@users.noreply.github.com>
2017-09-10 20:42:57 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
- 		var searchButton = document.querySelector(".button");
-	 	var openForm = document.querySelector(".form");
-	 	searchButton.addEventListener("click", function (evt) {
-	 	evt.preventDefault();
-	 	openForm.classList.toggle("form-show");});
+'use strict';
+
+    var searchButton = document.querySelector(".button");
+	var openForm = document.querySelector(".form");
+	searchButton.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	openForm.classList.toggle("form-show");});
