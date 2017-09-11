f98f024354c664d10c99167e6b8cdb5d7ad9fc5c
Elena Shagaeva <30930203+tommiknocker@users.noreply.github.com>
2017-09-11 21:33:56 +0300

    исправления

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
-    var searchButton = document.querySelector(".button");
+  var searchButton = document.querySelector(".button");
 	var openForm = document.querySelector(".form");
+
 	searchButton.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	openForm.classList.toggle("form-show");});
